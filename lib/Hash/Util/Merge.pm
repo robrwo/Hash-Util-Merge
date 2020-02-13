443cd70b04795d5fd6434c0cc19f058831ac48d9
Robert Rothenberg <rrwo@cpan.org>
2020-02-13 17:14:27 +0000

    Add some initial POD, adapted for Pod::Weaver

--- lib/Hash/Util/Merge.pm
+++ lib/Hash/Util/Merge.pm
@@ -5,6 +5,32 @@ use warnings;
 
 use Exporter ();
 
+=head1 NAME
+
+Hash::Util::Merge - utility functions for merging hashes
+
+=head1 SYNOPSIS
+
+  use Hash::Util::Merge qw/ mergemap /;
+
+  my %a = ( x => 1, y => 2 );
+  my %b = ( x => 3, y => 7 );
+
+  my $c = mergemap { $a + $b ) \%a, \%b;
+
+  # %c = ( x => 4, y => 9 );
+
+=head1 DESCRIPTION
+
+This module provides some syntactic sugar for merging simple
+hashes with a function.
+
+=head1 prepend:EXPORTS
+
+None by default.
+
+=cut
+
 our @EXPORT_OK = qw/ mergemap /;
 
 sub import {
@@ -20,6 +46,19 @@ sub import {
     ${"${pkg}::b"} = ${"${pkg}::b"};
     goto &Exporter::import;
 }
+
+=export mergemap
+
+  $hashref = mergemap { fn($a,$b) } \%a, \%b;
+
+For each key in the hashes C<%a> and C<%b>, this function applies the
+user-supplied function C<fn> to the corresponding values of that key,
+in the resulting hash reference.
+
+If a key does not exist in either of the hashes, then it will return
+C<undef>.
+
+=cut
 
 sub mergemap(&$$) {
 
@@ -41,5 +80,23 @@ sub mergemap(&$$) {
 
     return \%r;
 }
+
+=head1 KNOWN ISSUES
+
+L<Readonly> hashes, or those with locked keys, may return an error
+when merged with a hash that has other keys.
+
+=head1 SEE ALSO
+
+L<Hash::Merge>
+
+=head1 append:AUTHOR
+
+This module uses code from L<List::Util::PP>.
+
+This module was developed from work for Science Photo Library
+L<https://www.sciencephoto.com>.
+
+=cut
 
 1;
